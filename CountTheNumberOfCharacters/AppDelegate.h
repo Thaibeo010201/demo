0e859f96efc30ec0811f399766e33eabf610d437
Le Duc <ducld0701@gmail.com>
2023-07-08 19:42:37 +0700

    Initial Commit

---
+++ CountTheNumberOfCharacters/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CountTheNumberOfCharacters
+//
+//  Created by Le Yen on 08/07/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CountTheNumberOfCharacters/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CountTheNumberOfCharacters
+//
+//  Created by Le Yen on 08/07/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
